08c6ae1003410ba8415a7bcd69fbef9649806484
Mike Spears <mike.spears@utoronto.ca>
2019-09-24 14:35:26 -0400

    created basic interface

--- SecretCode/ViewController.swift
+++ SecretCode/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var onOff: UISwitch!
+    @IBOutlet weak var segmented: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        slider.value = 0.0
+        sliderLabel.text = "\(slider.value)"
+        onOff.isOn = false
+        segmented.selectedSegmentIndex = 0
+        
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        sliderLabel.text = "\(Int(slider.value * 10.0))"
+    }
+    
+    
+    @IBAction func checkAction(_ sender: Any) {
+    }
 }
 
