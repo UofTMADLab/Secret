eaa368e78e48e72dc06dc683d0af77627c39f97e
Mike Spears <mike.spears@utoronto.ca>
2019-09-24 14:00:26 -0400

    Initial Commit

---
+++ SecretCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecretCode
+//
+//  Created by Mike Spears on 2019-09-24.
+//  Copyright © 2019 Mike Spears. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
